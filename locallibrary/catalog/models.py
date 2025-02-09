de39409d608bd7a68b6c858d0b0e4804da1b5a99
Brendan <hombm@dukes.jmu.edu>
2025-02-09 11:31:13 -0500

    Part 4

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -58,6 +58,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
@@ -91,7 +97,6 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
-
 
 class Author(models.Model):
     """Model representing an author."""
