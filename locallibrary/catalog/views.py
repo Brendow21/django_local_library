99ba0b8c0283f02ef86bbfc59bac110923ddbdbe
Brendan Hom <hombm@dukes.jmu.edu>
2025-02-21 19:43:02 -0500

    Part 6: Lists, Deatail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -17,12 +14,40 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of genres
+    num_genres = Genre.objects.count()
+
+    # Number of books containing the word "the"
+    num_books_the = Book.objects.filter(title__contains='the').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_the': num_books_the,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='the')[:5] # Get 5 books containing the title war
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
